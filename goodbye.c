8a733e87616e2ad13f46894f808837a4549084c0
vensder <dmitrii.makarov@mattr.global>
2025-10-24 15:10:27 +1300

    feat: updated ci, readme, formatted code (#1)
    * feat: updated ci, readme, formatted code
    
    * feat(ci): updated workflow
    
    * chore(ci): updated path-ignore

--- goodbye.c
+++ goodbye.c
@@ -2,19 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_goodbye (void)
+void print_goodbye(void)
 {
-  const char * msg = "Goodbye World!\n";
+  const char *msg = "Goodbye World!\n";
   int printf_res = printf(msg);
   if (printf_res < strlen(msg))
 
   {
     return 1;
-
-  } else {
-
-    return 0;
-
   }
-
+  else
+  {
+    return 0;
+  }
 }
